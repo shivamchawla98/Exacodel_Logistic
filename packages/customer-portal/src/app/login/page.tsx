308fd3f4ea027a3ba390ce67e6b1c8568909aa6b
Abhishek Bharti <abhishekbharti9910@gmail.com>
2023-09-20 17:04:20 +0530

    small change in login form

--- packages/customer-portal/src/app/login/page.tsx
+++ packages/customer-portal/src/app/login/page.tsx
@@ -36,7 +36,7 @@ function Page() {
   return (
     <div className="h-3/4 bg-white py-6 flex flex-col justify-center sm:py-12">
     <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-      <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+      <div className="absolute inset-0 bg-gradient-to-r from-sky-300 to-sky-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
       <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
         <div className="max-w-md mx-auto">
           <div>
@@ -99,7 +99,7 @@ function Page() {
                   <div className="relative">
                     <button
                       type="submit"
-                      className="bg-blue-500 hover:bg-blue-700 text-white rounded-md px-2 py-1"
+                      className="bg-sky-500 hover:bg-sky-700 text-white rounded-md px-2 py-1"
                       disabled={isSubmitting}
                     >
                       {isSubmitting ? 'Submitting...' : 'Submit'}
