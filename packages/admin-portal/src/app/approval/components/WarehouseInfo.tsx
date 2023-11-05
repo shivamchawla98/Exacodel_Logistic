679b7b8b03f9e413c1f7f389805009d8fb5c4ece
Abhishek Bharti <abhishekbharti9910@gmail.com>
2023-11-05 11:05:46 +0530

    minor changes

--- packages/admin-portal/src/app/approval/components/WarehouseInfo.tsx
+++ packages/admin-portal/src/app/approval/components/WarehouseInfo.tsx
@@ -107,7 +107,7 @@ export default function WarehouseInfo({ Id }: any) {
                         type="text"
                         disabled
                         value={value}
-                        className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:border-sky-500 text-sm text-gray-700 placeholder-gray-400"
+                        className=" rounded-md px-3 py-2 w-full focus:outline-none text-sm text-gray-700 placeholder-gray-400"
                       />
 
                     </div>
